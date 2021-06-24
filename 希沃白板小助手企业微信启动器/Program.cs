21a4a950fab6a785a12823891be5cabaa13f05ff
walterlv <xblero@gmail.com>
2021-06-24 18:31:44 +0800

    Add project files.

---
+++ 希沃白板小助手企业微信启动器/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+using Microsoft.Win32;
+
+using Walterlv.Win32;
+
+namespace Walterlv.Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                RunOnWindows();
+            }
+        }
+
+        [SupportedOSPlatform("Windows")]
+        private static void RunOnWindows()
+        {
+            var wxPath = RegistryHive.CurrentUser.Read64(
+                @"SOFTWARE\Tencent\WXWork",
+                "Executable");
+            if (!string.IsNullOrWhiteSpace(wxPath) && File.Exists(wxPath))
+            {
+                RegistryHive.CurrentUser.Write64(
+                    @"SOFTWARE\Tencent\WXWork",
+                    "multi_instances",
+                    2);
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = wxPath,
+                    UseShellExecute = false,
+                });
+            }
+        }
+    }
+}
